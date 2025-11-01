c2f2ca70450fa0e4e299ab00993a47432d88036f
Paula Danthu <paula.danthu@cegelec.com>
2025-11-01 19:13:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/timeline/',
+  base: '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
