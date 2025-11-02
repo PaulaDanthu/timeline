11c7b952ec070be5976d813327197734443cd71b
Paula Danthu <paula.danthu@cegelec.com>
2025-11-02 18:20:03 +0100

    debugging

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/timeline',
+  base: '/timeline/', // include trailing slash
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
 })
